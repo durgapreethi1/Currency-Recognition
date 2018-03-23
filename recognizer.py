35c4f8d50537d77e87ad438f38e2d57c2b622de8
Kandula Durga Preethi <15wh1a0513@bvrithyderabad.edu.in>
2018-03-23 20:22:37 +0530

    Add files via upload

---
+++ recognizer.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+recognizer = cv2.face.LBPHFaceRecognizer_create() 
+recognizer.read('C:\\Python27\\Facerecognition\\trainer\\trainer.yml')
+cascadePath = "C:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+cam = cv2.VideoCapture(0)
+font =  cv2.FONT_HERSHEY_SIMPLEX
+
+while True:
+    ret, im =cam.read()
+    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    faces=faceCascade.detectMultiScale(gray, 1.2,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
+        Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+       
+        if(Id==1):
+            Id="Family Member,Access Granted"
+        elif(Id==2):
+                Id="Sam"
+        else:
+            Id="Unknown"
+        cv2.putText(im,str(Id), (x,y+h),font,1,(255,255,255))
+ 
+    cv2.imshow('im',im) 
+    if cv2.waitKey(10) & 0xFF==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
